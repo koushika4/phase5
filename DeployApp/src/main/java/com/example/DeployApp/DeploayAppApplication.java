8e2777e5e847319f9888e969069cf6fda1c04bb3
koushikabalusu <koushika1204@sasi.ac.in>
2022-10-06 18:33:21 +0530

    changes

---
+++ DeployApp/src/main/java/com/example/DeployApp/DeploayAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.DeployApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploayAppApplication.class, args);
+	}
+
+}
